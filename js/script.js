20e9505e8076e47d8f32ef559de659a04a7574f3
musabbirrahman <musabbirrahman490@gmail.com>
2026-02-24 00:33:34 +0600

    created filter section and function and appended it for interview

--- js/script.js
+++ js/script.js
@@ -1,36 +1,102 @@
-let totalCount = document.getElementById('total-count');
-let interviewCount = document.getElementById('interview-count');
-let rejectedCount = document.getElementById('rejected-count');
-let allJobsCount = document.getElementById('all-jobs');
-
 let interviewArr = [];
 let rejectedArr = [];
 
-let mainList = document.getElementsByTagName('main')
+let totalCount = document.getElementById("total-count");
+let interviewCount = document.getElementById("interview-count");
+let rejectedCount = document.getElementById("rejected-count");
+let allJobsCount = document.getElementById("all-jobs");
 
-
-function countCalculate(){
-    totalCount.innerText = allJobsCount.children.length;
-    interviewCount.innerText = interviewArr.length;
-    rejectedCount.innerText = rejectedArr.length;
+function countCalculate() {
+  totalCount.innerText = allJobsCount.children.length;
+  interviewCount.innerText = interviewArr.length;
+  rejectedCount.innerText = rejectedArr.length;
 }
 
-countCalculate()
+countCalculate();
 
-const btnAllFilter = document.getElementById('btn-all-filter');
-const btnInterviewFilter = document.getElementById('btn-interview-filter');
-const btnRejectedFilter = document.getElementById('btn-rejected-filter');
+const btnAllFilter = document.getElementById("btn-all-filter");
+const btnInterviewFilter = document.getElementById("btn-interview-filter");
+const btnRejectedFilter = document.getElementById("btn-rejected-filter");
 
-function toggleBtn(id){
-    btnAllFilter.classList.remove('btn-primary');
-    btnInterviewFilter.classList.remove('btn-primary');
-    btnRejectedFilter.classList.remove('btn-primary');
+function toggleBtn(id) {
+  btnAllFilter.classList.remove("btn-primary");
+  btnInterviewFilter.classList.remove("btn-primary");
+  btnRejectedFilter.classList.remove("btn-primary");
 
-    btnAllFilter.classList.add('text-[#64748B]');
-    btnInterviewFilter.classList.add('text-[#64748B]');
-    btnRejectedFilter.classList.add('text-[#64748B]');
+  btnAllFilter.classList.add("text-[#64748B]");
+  btnInterviewFilter.classList.add("text-[#64748B]");
+  btnRejectedFilter.classList.add("text-[#64748B]");
 
-    const btnPrimary = document.getElementById(id);
-    btnPrimary.classList.add('btn-primary');
-    btnPrimary.classList.remove('text-[#64748B]');
+  const btnPrimary = document.getElementById(id);
+  btnPrimary.classList.add("btn-primary");
+  btnPrimary.classList.remove("text-[#64748B]");
+}
+
+const mainList = document.querySelector("main");
+
+mainList.addEventListener("click", function (event) {
+    
+  if (event.target.classList.contains("btn-interview")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const jobPosition = parentNode.querySelector(".job-position").innerText;
+    const jobInfo = parentNode.querySelector(".job-info").innerText;
+    const jobStatus = parentNode.querySelector(".status-btn").innerText;
+    const jobDescription =
+      parentNode.querySelector(".job-description").innerText;
+
+    const jobObj = {
+      companyName,
+      jobPosition,
+      jobInfo,
+      jobStatus,
+      jobDescription,
+    };
+    
+    const jobSearch = interviewArr.find(
+      (item) => item.companyName == jobObj.companyName,
+    );
+
+    if (!jobSearch) {
+      interviewArr.push(jobObj);
+    }
+
+    renderInterview();
+  }
+});
+
+const filterSection = document.getElementById("filter-section");
+function renderInterview() {
+
+  for (let interview of interviewArr) {
+    console.log(interview)
+
+    filterSection.innerHTML = ``;
+    let div = document.createElement("div");
+    div.classList = "bg-[#FFFFFF] p-6 rounded-md flex justify-between job-card";
+    div.innerHTML = `
+                    <div>
+                        <h3 class="mb-1 text-[#002C5C] text-[18px] font-semibold leading-7 company-name">Mobile First Corp</h3>
+                        <p class="mb-5 text-[#64748B] leading-6 job-position">React Native Developer</p>
+                        <p class="mb-5 text-[#64748B] text-[14px] leading-5 job-info">Remote
+                            •
+                            Full-time
+                            •
+                            $130,000 - $175,000</p>
+                        <p class="status-btn btn mb-2">Not Applied</p>
+                        <p class="mb-5 text-[#323B49] text-[14px] leading-5 job-description">Build cross-platform mobile applications
+                            using React Native. Work on products used by millions of users worldwide.</p>
+                        <div>
+                            <button class="btn btn-outline btn-accent btn-interview">Interview</button>
+                            <button class="btn btn-outline btn-secondary btn-rejected">Rejected</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn-delete btn btn-ghost rounded-full"><i class="fa-regular fa-trash-can"></i></button>
+
+                    </div>
+        
+    `;
+    filterSection.appendChild(div);
+  }
 }
